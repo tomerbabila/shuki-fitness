64003fe4687135a9d24d631fedced240885144b0
tomerbabila <tomer.babila@gmail.com>
2023-10-16 19:02:33 +0300

    Create workout component

---
+++ src/app/shared/components/workout/workout.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  templateUrl: './workout.component.html',
+  styleUrls: ['./workout.component.scss'],
+})
+export class WorkoutComponent {
+  @Input() title!: string;
+  @Input() desc!: string;
+  @Input() date!: Date;
+  @Input() currentMembers = 0;
+  @Input() totalMembers!: number;
+}
---
+++ src/app/shared/components/workout/workout.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkoutComponent } from './workout.component';
+
+@NgModule({
+  declarations: [WorkoutComponent],
+  imports: [CommonModule],
+})
+export class WorkoutModule {}
