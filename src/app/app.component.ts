9e50ee82f1393a56f2c90a2a28c263e168b52636
tomerbabila <tomer.babila@gmail.com>
2023-10-17 12:40:39 +0300

    Add multi languages support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { TextDirectionService } from './shared/services/text-direction/text-direction.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,17 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    private textDirectionService: TextDirectionService,
+    private renderer: Renderer2
+  ) {
+    // TODO: Change default language to Hebrew
     translate.setDefaultLang('en');
     translate.use('en');
+
+    this.textDirectionService.textDirection$.subscribe(dir => {
+      this.renderer.setAttribute(document.body, 'dir', dir);
+    });
   }
 }
---
+++ src/app/shared/services/text-direction/text-direction.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TextDirectionEnum } from './text-direction.types';
+import { rtlLanguages } from './text-direction.constants';
+
+@Injectable({ providedIn: 'root' })
+export class TextDirectionService {
+  private _textDirection = new BehaviorSubject(TextDirectionEnum.ltr);
+  public textDirection$ = this._textDirection.asObservable();
+
+  constructor() {
+    this.checkDirection();
+  }
+
+  checkDirection() {
+    const lang = 'en'; // TODO: add localStorage support here
+
+    if (this.isRtl(lang)) {
+      this._textDirection.next(TextDirectionEnum.rtl);
+    } else {
+      this._textDirection.next(TextDirectionEnum.ltr);
+    }
+  }
+
+  private isRtl(lang: string) {
+    return rtlLanguages.includes(lang);
+  }
+}
