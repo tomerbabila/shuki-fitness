8ec7c706129727772c74f9941d24845bba6cc9fc
tomerbabila <tomer.babila@gmail.com>
2023-10-19 14:27:44 +0300

    Setup api for firestore

--- src/app/shared/store/api.ts
+++ src/app/shared/store/api.ts
@@ -1,1 +1,19 @@
-export class ApiAdapter {}
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+
+function firebaseSerialize<T>(object: T) {
+  return JSON.parse(JSON.stringify(object));
+}
+
+export class FirestoreCrudService<T> {
+  private collection: AngularFirestoreCollection<T>;
+
+  constructor(
+    private afs: AngularFirestore,
+    collectionName: string
+  ) {
+    this.collection = this.afs.collection(collectionName);
+  }
+}
