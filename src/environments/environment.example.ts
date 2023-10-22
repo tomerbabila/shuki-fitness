aa19b18d223afceb2a9d6d98bd812f33f6e1ff04
tomerbabila <tomer.babila@gmail.com>
2023-10-22 17:17:13 +0300

    Fix environments

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'project id',
+    appId: 'app id',
+    storageBucket: 'url of bucket',
+    apiKey: 'firebase api key',
+    authDomain: 'auth domain',
+    messagingSenderId: 'messaging sender id',
+    measurementId: 'mesurement id',
+  },
+};
