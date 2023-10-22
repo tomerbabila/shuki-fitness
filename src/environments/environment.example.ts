a4fd0a5fc55e3fafe3a75afe3340ad871bf25720
tomerbabila <tomer.babila@gmail.com>
2023-10-22 18:58:30 +0300

    Update environment.example.ts

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'project id',
-    appId: 'app id',
-    storageBucket: 'url of bucket',
-    apiKey: 'firebase api key',
-    authDomain: 'auth domain',
-    messagingSenderId: 'messaging sender id',
-    measurementId: 'mesurement id',
+    projectId: '[Project Id]',
+    appId: '[App Id]',
+    storageBucket: '[Project Id].appspot.com',
+    apiKey: '[Project Key]',
+    authDomain: '[Project Id].firebaseapp.com',
+    messagingSenderId: '[messageSender Id]',
+    measurementId: '[Measurement Id]',
   },
 };
