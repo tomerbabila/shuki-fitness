0f54262761b71ca8bc2e4cb4d31ea8b93a0a9f08
tomerbabila <tomer.babila@gmail.com>
2023-10-22 18:58:48 +0300

    Update auth.ts methods

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,16 +1,75 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signOut,
+  Auth,
+} from '@angular/fire/auth';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(private auth: Auth) {}
 
-  login(phoneNumber: string) {
-    const appVerifier = window.recaptchaVerifier;
+  loginWithEmail(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
 
-    this.auth
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then(console.log)
-      .catch(console.log);
+        console.log(user);
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log({ errorCode, errorMessage });
+      });
+  }
+
+  loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider)
+      .then(result => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+
+        console.log({ credential, token, user });
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+        console.log({ errorCode, errorMessage, email, credential });
+      });
+  }
+
+  register(email: string, password: string) {
+    return createUserWithEmailAndPassword(this.auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+
+        console.log(user);
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log({ errorCode, errorMessage });
+      });
+  }
+
+  logout() {
+    return signOut(this.auth)
+      .then(() => {
+        console.log('Sign-out successful.');
+      })
+      .catch(error => {
+        console.log(error);
+        console.log('An error happened.');
+      });
   }
 }
